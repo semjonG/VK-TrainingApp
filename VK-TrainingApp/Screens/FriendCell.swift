d73d0f15d5a05ccd79a65e94e2e89fbba5ed0623
SemjonG <simon.gerasimov@gmail.com>
2022-05-26 00:06:07 +0300

    FriendCell was updated

--- VK-TrainingApp/Screens/FriendCell.swift
+++ VK-TrainingApp/Screens/FriendCell.swift
@@ -23,10 +23,25 @@ class FriendCell: UITableViewCell {
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-        // рутовая view у ячеек - contentView
-        self.contentView.addSubview(nameLabel)
-        self.contentView.addSubview(photoImageView)
         
+        setupViews()
+        setupConstraints()
+    }
+    
+    // запасной инициализатор
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Private
+    
+    private func setupViews() {
+        // рутовая view у ячеек - contentView
+        contentView.addSubview(nameLabel)
+        contentView.addSubview(photoImageView)
+    }
+    
+    private func setupConstraints() {
         // нельзя устанавливать констрейнты, пока на вью не добавлен элемент
         NSLayoutConstraint.activate([
             
@@ -41,10 +56,5 @@ class FriendCell: UITableViewCell {
             nameLabel.leftAnchor.constraint(equalTo: photoImageView.rightAnchor, constant: 20),
             nameLabel.rightAnchor.constraint(equalTo: contentView.rightAnchor, constant: -20)
         ])
-    }
-    
-    // запасной инициализатор
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
     }
 }
